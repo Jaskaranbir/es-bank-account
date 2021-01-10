f8a1d7d9a6378218c33df56c5be67ef47923acb2
Jaskaranbir <jaskaranbir.dhillon@gmail.com>
2021-01-10 09:59:47 -0500

    Add GitHub-action to run tests

---
+++ .github/workflows/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -e
+set -x
+
+# =========================
+# Runs Ginkgo/Gomega tests
+# =========================
+
+cd /tmp
+
+echo "Installing Ginkgo and Gomega..."
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega/...
+
+cd $GOPATH/src/github.com/Jaskaranbir/es-bank-account
+
+echo "Running go tests"
+ginkgo -r \
+      --v \
+      --race \
+      --trace \
+      --progress \
+      --failOnPending \
+      --randomizeSuites \
+      --randomizeAllSpecs
+      # TODO: Integrate with codecov
+      #  --cover \
+      #  -coverprofile=coverage.txt \
+      #  -outputdir=$(pwd)
